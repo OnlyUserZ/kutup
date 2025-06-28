5d12499d100fc11092ab5352ec193debfb27708e
Kenan Şeniz <kwortex666@gmail.com>
2025-06-28 16:26:00 +0300

    Update KimlikBilgisi.java

--- src/main/java/com/veritabani/kutup/KimlikBilgisi.java
+++ src/main/java/com/veritabani/kutup/KimlikBilgisi.java
@@ -17,6 +17,3 @@ public class KimlikBilgisi {
     @JoinColumn(name = "kullanici_id")
     private Kullanici kullanici;
 }
-//Hangi taraf diğerine bağımlıysa joincolumn (SQL de foreign key) oraya koyulur mesela kullanıcı olmadan adres varolamaz
-//ama adres olmadan o kullanıcı varolabilir burada bağımlı sınıf adrestir o yüzden bağımlı olana joincolumn koyuyoruz
-//mesela insanın annesiyle bağı olsun ben annem ölüncede varolurum ben olmazsam zaten annem diye bir şey olmaz buradada anneye joincolumn koyarız
